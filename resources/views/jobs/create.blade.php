72b7b99383f39f662c92ca161bfdb8c9528f1ab8
CodyJReed <cody_j_reed@proton.me>
2026-05-26 18:12:04 -0400

    view template excerise

---
+++ resources/views/jobs/create.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Job Listings</title>
+</head>
+<body>
+    <h1>Job Creation</h1>
+    <form>
+        <input name="test"/>
+        <button type="submit">Create Job</button>
+    </form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,22 +7,9 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return '<h1>Available Jobs.</h1>';
+    return view('jobs.index');
 });
-// Response class/response helper exercise
-Route::get('/text', function() {
-    return response()->json([
-        'title' => 'test'
-    ], 201)->cookie('name', 'jane doe');
-});
-// Downloads
-Route::get('/download', function() {
-    return response()->download(public_path('favicon.ico'));
-});
-// Cookie retrieval
-Route::get('/read-cookie', function(Request $request) {
-    $cookieValue = $request->cookie('name');
-    return response()->json([
-        'cookie' => $cookieValue
-    ]);
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
