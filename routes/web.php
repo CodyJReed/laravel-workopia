262026f6fdf9881d50b5da0e4ddfb0591898b63b
CodyJReed <cody_j_reed@proton.me>
2026-05-26 17:26:59 -0400

    Route class methods exercise

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,10 +11,24 @@ Route::get('/jobs', function () {
     return '<h1>Available Jobs.</h1>';
 });
 
-Route::get('/posts/{id}', function (string $id) {
-    return 'Post ' . $id;
-});
 
-Route::get('/posts/{id}/comments/{commentId}', function (string $id, string $commentId) {
-    return 'Post ' . $id . ' Coment ' . $commentId;
+// Exercise with $request class
+Route::get('/tests', function (Request $request) {
+    return [
+        'method' => $request->method(),
+        'url' => $request->url(),
+        'path' => $request->path(),
+        'fullUrl' => $request->fullUrl(),
+        'ip' => $request->ip(),
+        'userAgent' => $request->userAgent(),
+        'header' => $request->header(),
+    ];
+});
+// Query params exercise
+Route::get('/query', function (Request $request) {
+    // ($query) | only($query) | all() |
+    //  has($query) |
+    //  input($query) <- works with forms && requests
+    // except($query) <- filter out provide array
+    return $request->all();
 });
