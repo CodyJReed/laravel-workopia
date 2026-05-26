1a7c836fba230282f63306be24f015a75de03ead
CodyJReed <cody_j_reed@proton.me>
2026-05-26 16:57:40 -0400

    routes exercise

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function () {
+    return '<h1>Available Jobs.</h1>';
+});
+
+Route::get('/posts/{id}', function (string $id) {
+    return 'Post ' . $id;
+});
+
+Route::get('/posts/{id}/comments/{commentId}', function (string $id, string $commentId) {
+    return 'Post ' . $id . ' Coment ' . $commentId;
+});
