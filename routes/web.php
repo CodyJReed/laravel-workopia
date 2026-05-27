379aab1fee90e83f1dec80a60cf03a57fbb22918
CodyJReed <cody_j_reed@proton.me>
2026-05-27 14:37:28 -0400

    data passing through routing views

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,25 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs.index');
+    $title = 'Available Jobs.';
+    $jobs = [
+        'Software Dev',
+        'DB Admin',
+        'Software Eng',
+        'Systems Analyst'
+    ];
+    // Data/State can be pass to templates/pages in a variaty of different ways...
+    // As an associative_array within the 'view' method
+
+    // return view('jobs.index', [
+    //     'title' => $title
+    // ]);
+
+    // ...as comma separated pairs attached to a 'with' method of view
+    // return view('jobs.index')->with('title', $title);
+
+    //...using the compact method with the 'view' method
+    return view('jobs.index', compact('title', 'jobs'));
 });
 
 Route::get('/jobs/create', function () {
