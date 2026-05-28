008dd74b23300c64ab305191e86b740dc6a009f2
CodyJReed <cody_j_reed@proton.me>
2026-05-28 18:25:39 -0400

    controller exercise and routes refactor

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class JobController extends Controller
+{
+    public function index()
+    {
+        $title = 'Available Jobs.';
+        $jobs = [
+            'Software Dev',
+            'DB Admin',
+            'Software Eng',
+            'Systems Analyst'
+        ];
+        // Data/State can be pass to templates/pages in a variaty of different ways...
+        // As an associative_array within the 'view' method
+
+        // return view('jobs.index', [
+        //     'title' => $title
+        // ]);
+
+        // ...as comma separated pairs attached to a 'with' method of view
+        // return view('jobs.index')->with('title', $title);
+
+        //...using the compact method with the 'view' method
+        return view('jobs.index', compact('title', 'jobs'));
+    }
+
+    public function create()
+    {
+        return view('jobs.create');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/jobs', function () {
-    $title = 'Available Jobs.';
-    $jobs = [
-        'Software Dev',
-        'DB Admin',
-        'Software Eng',
-        'Systems Analyst'
-    ];
-    // Data/State can be pass to templates/pages in a variaty of different ways...
-    // As an associative_array within the 'view' method
-
-    // return view('jobs.index', [
-    //     'title' => $title
-    // ]);
-
-    // ...as comma separated pairs attached to a 'with' method of view
-    // return view('jobs.index')->with('title', $title);
-
-    //...using the compact method with the 'view' method
-    return view('jobs.index', compact('title', 'jobs'));
-});
-
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
+Route::get('/jobs', [JobController::class, 'index']);
+Route::get('/jobs/create', [JobController::class, 'create']);
