f4c41ccbe2e01a977f301e0fa62e29d6fbfff105
CodyJReed <cody_j_reed@proton.me>
2026-05-29 14:33:01 -0400

    add styling to header component; include font-awesome cdn link

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,14 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{$title ?? 'Laravel | Workopia'}}</title>
+    <title>{{ $title ?? 'Laravel | Workopia' }}</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
+        integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/css/app.css')
 </head>
 
 <body class="bg-gray-100">
-    <x-header/>
+    <x-header />
     <main class="container mx-auto p-4 mt-4">
-        {{$slot}}
+        {{ $slot }}
     </main>
 </body>
 
