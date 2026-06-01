d4010a59947a804f34b8589839b0588b310ad9d6
CodyJReed <cody_j_reed@proton.me>
2026-06-01 12:26:43 -0400

    model introduction exercise

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -12,12 +13,7 @@ class JobController extends Controller
      */
     public function index(): View
     {
-        $jobs = [
-            "Web Developer",
-            "DB Admin",
-            "Software Engineer",
-            "Systems Analyst"
-        ];
+        $jobs = Job::all();
 
         return view('jobs.index', compact('jobs'));
     }
