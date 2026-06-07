abb3ef8f84cbe6de5457484e0164a521c743449c
CodyJReed <cody_j_reed@proton.me>
2026-06-06 23:45:18 -0400

    Add register && login controller(s); add initial routing && renders

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,9 @@ use App\Models\Job;
 use Illuminate\View\View;
 
 class HomeController extends Controller
-{
+{   
+    // @desc Show Home page
+    // @route GET /index
     public function index(): View
     {
         $jobs = Job::latest()->limit(6)->get();
