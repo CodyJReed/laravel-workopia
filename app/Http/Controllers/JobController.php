6d20caad40dcd7f69aa4038c30dffbd3c86af959
CodyJReed <cody_j_reed@proton.me>
2026-06-07 20:52:42 -0400

    clean up unused import; add auth, only && except to 'job' routes

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Job;
-use Illuminate\Contracts\Support\ValidatedData;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
