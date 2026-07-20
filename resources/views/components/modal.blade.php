f5e1dab517649099874e7003f96fd3cdfd687561
CodyJReed <cody_j_reed@proton.me>
2026-07-20 15:56:33 -0400

    fix for apline js pre-render

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -5,7 +5,7 @@
         class="block w-full text-center px-5 py-2.5 shadow-sm rounded border text-base font-medium cursor-pointer text-indigo-700 bg-indigo-100 hover:bg-indigo-200">
         {{ $buttonText }}
     </button>
-    <div class="fixed inset-0 flex items-center justify-center bg-gray-900/50" x-show="open">
+    <div x-cloak class="fixed inset-0 flex items-center justify-center bg-gray-900/50" x-show="open">
         <div @click.away="open = false" class="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
             {{ $slot }}
         </div>
